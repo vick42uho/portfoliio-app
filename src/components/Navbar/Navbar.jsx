5d6c9106c52cb389794fb09d402723f8b1fdd550
Wick <50046154+vick42uho@users.noreply.github.com>
2024-05-19 13:56:41 +0700

    Update Navbar.jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -17,7 +17,7 @@ function Navbar() {
         <div className={styles.container}>
           <div className={styles.nav_con}>
             <div className={styles.logo}>
-              <a href="#">JD PORT</a>
+              <a href="#">JD PORT Wick</a>
             </div>
             <ul>
               <li><a href="#services">Skills</a></li>
